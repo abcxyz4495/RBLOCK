1460970084722165bb4a4493a0120160ab5215eb
abcxyz4495 <abcxyz4495@gmail.com>
2024-02-16 17:54:33 +0530

    Updated URL

--- backend/src/configs/allowedOrigins.js
+++ backend/src/configs/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
 	"https://rblock-backend.onrender.com",
+	"https://rblock-backend.onrender.com/auth/login",
+	"https://rblock-backend.onrender.com/auth/refresh",
 ];
 
 module.exports = allowedOrigins;
